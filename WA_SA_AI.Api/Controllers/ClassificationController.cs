383890d9441ea7bcd2fa22ecc5efacea56b5e302
nanfeng <cheongnanfeng@gmail.com>
2018-09-22 19:14:56 +0800

    Add api project

---
+++ WA_SA_AI.Api/Controllers/ClassificationController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WA_SA_AI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassificationController : ControllerBase
+    {
+        // GET: api/Classification
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Classification/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Classification
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Classification/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WA_SA_AI.Api/Services/TagServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WA_SA_AI.Api.Services
+{
+    public class TagServices
+    {
+    }
+}
